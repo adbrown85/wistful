1f52cddd5954cea04a31d0e7f565717c6acbcc18
Andy Brown <adb1413@rit.edu>
2011-03-06 13:43:32 -0500

    Test for GlWindowGlx

---
+++ test/GlWindowGlxTest.cxx
@@ -0,0 +1,32 @@
+/*
+ * GlWindowGlxTest.cxx
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "GlWindowGlx.hpp"
+#include <iostream>
+using namespace std;
+
+
+/** Test for GlWindowGlx. */
+class GlWindowGlxTest {
+public:
+	void testShow();
+};
+
+/** Ensures window can be shown. */
+void GlWindowGlxTest::testShow() {
+	
+	GlWindowGlx window;
+	
+	window.show();
+}
+
+/** Runs the test. */
+int main(int argc, char *argv[]) {
+	
+	GlWindowGlxTest test;
+	
+	test.testShow();
+}
