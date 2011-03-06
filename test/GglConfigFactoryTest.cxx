063f3d880a9854d7c9a8104f6ff2a1972cec82de
Andy Brown <adb1413@rit.edu>
2011-03-06 18:38:36 -0500

    Updated tests for renames

--- test/GglConfigFactoryTest.cxx
+++ test/GglConfigFactoryTest.cxx
@@ -1,28 +1,28 @@
 /*
- * GlConfigFactoryTest.cxx
+ * GglConfigFactoryTest.cxx
  * 
  * Author
  *     Andrew Brown <adb1413@rit.edu>
  */
 
 
-/** Test for GlConfigFactory. */
-class GlConfigFactoryTest {
+/** Test for GglConfigFactory. */
+class GglConfigFactoryTest {
 public:
 	void testCreate();
 };
 
 
 /** Ensures configurations can be created correctly. */
-void GlConfigFactoryTest::testCreate() {
+void GglConfigFactoryTest::testCreate() {
 	
-	GlConfigFactory glcf;
+	GglConfigFactory cf;
 }
 
 /** Runs the test. */
 int main(int argc, char *argv[]) {
 	
-	GlConfigFactoryTest test;
+	GglConfigFactoryTest test;
 	
 	test.testCreate();
 	return 0;
