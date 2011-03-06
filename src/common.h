da4af80c7d930058fdb2f0441972f7192f8ffb29
Andy Brown <adb1413@rit.edu>
2011-03-06 13:40:44 -0500

    Added common header file

---
+++ src/common.h
@@ -0,0 +1,18 @@
+/*
+ * common.h
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef COMMON_H
+#define COMMON_H
+#include <cstdlib>
+#include <iostream>
+#include <list>
+#include <map>
+#include <GL/gl.h>
+#include <GL/glx.h>
+using namespace std;
+
+
+#endif
