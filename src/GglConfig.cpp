fb169af8e1d33b71949661018366cef74aef6f64
Andy Brown <adb1413@rit.edu>
2011-03-06 22:34:47 -0500

    GglConfig abstract

--- src/GglConfig.cpp
+++ src/GglConfig.cpp
@@ -6,29 +6,106 @@
  */
 #include "GglConfig.hpp"
 
+/** Creates an OpenGL configuration builder. */
+GglConfigBuilder::GglConfigBuilder() {
+	r = DEFAULT_R;
+	g = DEFAULT_G;
+	b = DEFAULT_B;
+	a = DEFAULT_A;
+}
+
+/** Reverts all values to their defaults. */
+void GglConfigBuilder::reset() {
+	r = DEFAULT_R;
+	g = DEFAULT_G;
+	b = DEFAULT_B;
+	a = DEFAULT_A;
+}
+
+/** Returns size of red buffer in bits. */
+int GglConfigBuilder::getRedSize() {
+	return r;
+}
+
+/** Changes the size of the red buffer. */
+void GglConfigBuilder::setRedSize(int r) {
+	this->r = r;
+}
+
+/** Returns size of green buffer in bits. */
+int GglConfigBuilder::getGreenSize() {
+	return g;
+}
+
+/** Changes the size of the green buffer. */
+void GglConfigBuilder::setGreenSize(int g) {
+	this->g = g;
+}
+
+/** Returns size of red buffer in bits. */
+int GglConfigBuilder::getBlueSize() {
+	return b;
+}
+
+/** Changes the size of the blue buffer. */
+void GglConfigBuilder::setBlueSize(int b) {
+	this->b = b;
+}
+
+/** Returns size of red buffer in bits. */
+int GglConfigBuilder::getAlphaSize() {
+	return a;
+}
+
+/** Changes the size of the alpha buffer. */
+void GglConfigBuilder::setAlphaSize(int a) {
+	this->a = a;
+}
+
 /** Creates an OpenGL configuration. */
-GglConfig::GglConfig(GLXFBConfig fbc) {
-	this->fbConfig = fbc;
+GglConfig::GglConfig(GglConfigBuilder *b) {
+	this->r = b->getRedSize();
+	this->g = b->getGreenSize();
+	this->b = b->getBlueSize();
+	this->a = b->getAlphaSize();
 }
 
-/** Destroys an OpenGL configuration. */
-GglConfig::~GglConfig() {
-	
+/** Returns size of red buffer in bits. */
+int GglConfig::getRedSize() const {
+	return r;
 }
 
-int GglConfig::getBufferSize() const {
-	return findAttribute(GLX_BUFFER_SIZE);
+/** Returns size of green buffer in bits. */
+int GglConfig::getGreenSize() const {
+	return g;
 }
 
-bool GglConfig::isDoubleBuffered() const {
-	return findAttribute(GLX_DOUBLEBUFFER) > 0;
+/** Returns size of red buffer in bits. */
+int GglConfig::getBlueSize() const {
+	return b;
 }
 
-/** Returns value of an attribute. */
-int GglConfig::findAttribute(int name) const {
-	
-	int value;
-	
-	glXGetFBConfigAttrib(display, fbConfig, name, &value);
-	return value;
+/** Returns size of red buffer in bits. */
+int GglConfig::getAlphaSize() const {
+	return a;
+}
+
+/** Changes the native framebuffer configuration with GLX. */
+void GglConfigGlxBuilder::setFBConfig(GLXFBConfig fbc) {
+	this->glxFBConfig = fbc;
+}
+
+/** Returns native framebuffer configuration with GLX. */
+GLXFBConfig GglConfigGlxBuilder::getFBConfig() {
+	return glxFBConfig;
+}
+
+/** Creates an OpenGL configuration with GLX. */
+GglConfigGlx::GglConfigGlx(GglConfigGlxBuilder *b) : GglConfig(b) {
+	this->glxFBConfig = b->getFBConfig();
+}
+
+/** Returns native framebuffer configuration for GLX. */
+GLXFBConfig GglConfigGlx::getFBConfig() {
+	return glxFBConfig;
 }
--- src/GglConfig.hpp
+++ src/GglConfig.hpp
@@ -9,15 +9,72 @@
 #include "common.h"
 
 
-/** Settings for how an OpenGL window is configured. */
+/** @brief <i>Builder</i> for @GglConfig. */
+class GglConfigBuilder {
+public:
+	GglConfigBuilder();
+	virtual ~GglConfigBuilder() {}
+	virtual void reset();
+// Getters and setters
+	virtual int getRedSize();
+	virtual void setRedSize(int r);
+	virtual int getGreenSize();
+	virtual void setGreenSize(int g);
+	virtual int getBlueSize();
+	virtual void setBlueSize(int b);
+	virtual int getAlphaSize();
+	virtual void setAlphaSize(int a);
+private:
+	int r;                                  // Red size
+	int g;                                  // Green size
+	int b;                                  // Blue size
+	int a;                                  // Alpha size
+// Constants
+	static const int DEFAULT_R = 8;
+	static const int DEFAULT_G = 8;
+	static const int DEFAULT_B = 8;
+	static const int DEFAULT_A = 8;
+};
+
+
+/** @brief OpenGL configuration. */
 class GglConfig {
 public:
-	GglConfig(GLXFBConfig fbc);
-	virtual ~GglConfig();
-	virtual int getBufferSize() const;
-	virtual bool isDoubleBuffered() const;
+	GglConfig(GglConfigBuilder *b);
+	virtual ~GglConfig() {}
+	virtual int getRedSize() const;
+	virtual int getGreenSize() const;
+	virtual int getBlueSize() const;
+	virtual int getAlphaSize() const;
 private:
-	GLXFBConfig fbConfig;
+	int r;                                  // Red size
+	int g;                                  // Green size
+	int b;                                  // Blue size
+	int a;                                  // Alpha size
 };
+
+
+/** @brief <i>Builder</i> for @GglConfigGlx. */
+class GglConfigGlxBuilder : public GglConfigBuilder {
+public:
+	virtual ~GglConfigGlxBuilder() {}
+// Getters and setters
+	virtual GLXFBConfig getFBConfig();
+	virtual void setFBConfig(GLXFBConfig fbc);
+private:
+	GLXFBConfig glxFBConfig;                // GLX Framebuffer configuration
+};
+
+
+/** @brief OpenGL configuration with GLX. */
+class GglConfigGlx : public GglConfig {
+public:
+	GglConfigGlx(GglConfigGlxBuilder *b);
+	virtual ~GglConfigGlx() {}
+	virtual GLXFBConfig getFBConfig();
+private:
+	GLXFBConfig glxFBConfig;                // GLX Framebuffer configuration
+};
+
 
 #endif
