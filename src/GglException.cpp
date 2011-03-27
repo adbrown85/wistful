63fd1f3879b0ad27db9cfba2e64cd659df127054
Andy Brown <adb1413@rit.edu>
2011-03-27 14:58:27 -0400

    Added GglException

---
+++ src/GglException.cpp
@@ -0,0 +1,30 @@
+/*
+ * GglException.cpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "GglException.hpp"
+
+/**
+ * Creates a GGL exception.
+ * 
+ * @param message Explanation shown to the user
+ */
+GglException::GglException(const string &message) throw() {
+    this->message = message;
+}
+
+/**
+ * Destroys the exception.
+ */
+GglException::~GglException() throw() {
+    ;
+}
+
+/**
+ * Returns the explanation that will be shown to the user.
+ */
+const char* GglException::what() const throw() {
+    return message.c_str();
+}
---
+++ src/GglException.hpp
@@ -0,0 +1,24 @@
+/*
+ * GglException.hpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GGLEXCEPTION_HPP
+#define GGLEXCEPTION_HPP
+#include "common.h"
+
+
+/**
+ * @brief Exception thrown from within GGL.
+ */
+class GglException : public exception {
+public:
+	GglException(const string &message) throw();
+	virtual ~GglException() throw();
+    virtual const char* what() const throw();
+private:
+    string message;
+};
+
+#endif
