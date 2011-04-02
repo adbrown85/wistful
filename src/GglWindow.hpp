cba8996f1fc1b7ed874e099332b759a52092a692
Andy Brown <adb1413@rit.edu>
2011-04-02 13:08:07 -0400

    Better documentation of GglWindow

--- src/GglWindow.hpp
+++ src/GglWindow.hpp
@@ -12,7 +12,35 @@
 #include "GglListener.hpp"
 
 
-/** @brief OpenGL window that can be shown. */
+/**
+ * @brief Abstract base class for an OpenGL window.
+ * 
+ * Because GGL is meant to keep things as simple as possible, currently
+ * <i>%GglWindow</i> encompasses both the OpenGL drawing area and the native
+ * operating system window itself.  Future releases may benefit from a cleaner
+ * separation, but right now it seems wise to not over-complicate things.
+ * 
+ * <i>%GglWindow</i> relies heavily on the <i>Template</i> design pattern.  It
+ * describes the base functionality for displaying an OpenGL window, and
+ * derived classes provide the concrete functionality for implementing that
+ * behavior by overriding <i>hooks</i>.  To make it easy to identify a hook, we
+ * follow the convention of naming the method with the prefix <i>do</i>.  For
+ * example, the hook for creating a native window is named
+ * <i>doCreateWindow</i>.
+ * 
+ * The process outlined by <i>%GglWindow</i> has four major steps, and there is
+ * a hook for each one.  Two of the steps deal with creating objects, and the
+ * other two handle cleaning them up.  The objects themselves are the native
+ * operating system window and the OpenGL context.  Therefore a derived class
+ * needs to implement creating and destroying the native window, as well as
+ * creating and destroying an OpenGL context with the native OpenGL
+ * interface.
+ * 
+ * In addition, a derived class also needs to implement <i>doGetEvent()</i>,
+ * which allows the window to process input from the user.  At the very least,
+ * a derived class should accept the event generated when the user closes the
+ * window.
+ */
 class GglWindow {
 public:
 	GglWindow();
