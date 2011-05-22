c90d088dff2ee57ae4bd16a569e6ec23bb47cb4e
Andy Brown <adb1413@rit.edu>
2011-05-21 22:12:52 -0400

    GglWindowCocoa implements GglWindow

--- src/window/cocoa/GglWindowCocoa.cpp
+++ src/window/cocoa/GglWindowCocoa.cpp
@@ -19,3 +19,47 @@ GglWindowCocoa::GglWindowCocoa() {
 GglWindowCocoa::~GglWindowCocoa() {
     ;
 }
+
+void GglWindowCocoa::doActivateContext() {
+    cout << "GglWindowCocoa::doActivateContext()" << endl;
+}
+
+void GglWindowCocoa::doActivateWindow() {
+    cout << "GglWindowCocoa::doActivateWindow()" << endl;
+}
+
+void GglWindowCocoa::doCreateConnection() throw(GglException) {
+    cout << "GglWindowCocoa::doCreateConnection()" << endl;
+}
+
+void GglWindowCocoa::doCreateWindow() throw(GglException) {
+    cout << "GglWindowCocoa::doCreateWindow()" << endl;
+}
+
+void GglWindowCocoa::doCreateContext() throw(GglException) {
+    cout << "GglWindowCocoa::doCreateContext()" << endl;
+}
+
+void GglWindowCocoa::doDestroyConnection() {
+    cout << "GglWindowCocoa::doDestroyConnection()" << endl;
+}
+
+void GglWindowCocoa::doDestroyContext() {
+    cout << "GglWindowCocoa::doDestroyContext()" << endl;
+}
+
+void GglWindowCocoa::doDestroyWindow() {
+    cout << "GglWindowCocoa::doDestroyWindow()" << endl;
+}
+
+void GglWindowCocoa::doFlush() {
+    cout << "GglWindowCocoa::doFlush()" << endl;
+}
+
+GglEvent GglWindowCocoa::doGetEvent() {
+    return GglEvent(OTHER);
+}
+
+void GglWindowCocoa::doRun() {
+    cout << "GglWindowCocoa::doRun()" << endl;
+}
--- src/window/cocoa/GglWindowCocoa.hpp
+++ src/window/cocoa/GglWindowCocoa.hpp
@@ -7,15 +7,27 @@
 #ifndef GGLWINDOWCOCOA_HPP
 #define GGLWINDOWCOCOA_HPP
 #include "ggl_common.h"
+#include "GglWindow.hpp"
 
 
 /**
  * @brief Window implemented with Cocoa.
  */
-class GglWindowCocoa {
+class GglWindowCocoa : public GglWindow {
 public:
     GglWindowCocoa();
     virtual ~GglWindowCocoa();
+    virtual void doActivateContext();
+    virtual void doActivateWindow();
+    virtual void doCreateConnection() throw (GglException);
+    virtual void doCreateWindow() throw (GglException);
+    virtual void doCreateContext()throw (GglException);
+    virtual void doDestroyConnection();
+    virtual void doDestroyContext();
+    virtual void doDestroyWindow();
+    virtual void doFlush();
+    virtual void doRun();
+    virtual GglEvent doGetEvent();
 };
 
 #endif
