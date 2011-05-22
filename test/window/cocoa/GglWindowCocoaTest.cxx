48faf550d0a3d6c09564b9a9e52200b76f527d01
Andy Brown <adb1413@rit.edu>
2011-05-21 20:33:40 -0400

    Better stub for GglWindowCocoaTest

--- test/window/cocoa/GglWindowCocoaTest.cxx
+++ test/window/cocoa/GglWindowCocoaTest.cxx
@@ -5,11 +5,30 @@
  *     Andrew Brown <adb1413@rit.edu>
  */
 #include "GglWindowCocoa.hpp"
+#include "GglFactory.hpp"
+using namespace std;
 
+/**
+ * Test for GglWindowCoca.
+ */
+class GglWindowCocoaTest {
+public:
+    void testOpen();
+};
+
+/**
+ * Ensures window can be opened.
+ */
+void GglWindowCocoaTest::testOpen() {
+    cout << "GglWindowCocoaTest::testOpen()" << endl;
+}
 
 /*
  * Runs the program.
  */
 int main(int argc, char *argv[]) {
-    cout << "Hello, World!" << endl;
+    
+    GglWindowCocoaTest test;
+    
+    test.testOpen();
 }
