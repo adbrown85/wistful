7d16ba6262c40faa1dad781878d616c44b33d0e0
Andy Brown <adb1413@rit.edu>
2011-06-17 19:35:12 -0400

    WindowGlx constructor uses Window(WindowFormat)

--- src/ggl/window/glx/GglWindowGlx.cpp
+++ src/ggl/window/glx/GglWindowGlx.cpp
@@ -13,7 +13,7 @@ long GglWindowGlx::DEFAULT_EVENT_MASK = getEventMask();
  * 
  * @param wf Container with window settings
  */
-GglWindowGlx::GglWindowGlx(const GglWindowFormat &wf) {
+GglWindowGlx::GglWindowGlx(const GglWindowFormat &wf) : GglWindow(wf) {
     this->display = NULL;
     this->info = NULL;
     this->window = (Window) NULL;
