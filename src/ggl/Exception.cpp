a2f1ced87de6d1a19d9c5ba45ee28258729f7d79
Andrew Brown <adb1413@rit.edu>
2011-06-20 22:17:10 -0400

    Use namespace Ggl in Exception.cpp

--- src/ggl/Exception.cpp
+++ src/ggl/Exception.cpp
@@ -5,26 +5,27 @@
  *     Andrew Brown <adb1413@rit.edu>
  */
 #include "ggl/Exception.hpp"
+using namespace Ggl;
 
 /**
  * Creates a GGL exception.
  * 
  * @param message Explanation shown to the user
  */
-Ggl::Exception::Exception(const std::string &message) throw() {
+Exception::Exception(const std::string &message) throw() {
     this->message = message;
 }
 
 /**
  * Destroys the exception.
  */
-Ggl::Exception::~Exception() throw() {
+Exception::~Exception() throw() {
     ;
 }
 
 /**
  * Returns the explanation that will be shown to the user.
  */
-const char* Ggl::Exception::what() const throw() {
+const char* Exception::what() const throw() {
     return message.c_str();
 }
