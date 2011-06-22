8db4286ed859d463fb88dc0ec1ce6fec6d29aa38
Andy Brown <adb1413@rit.edu>
2011-06-21 23:16:17 -0400

    VisualFactoryGlx beginnings

---
+++ src/ggl/window/glx/VisualFactoryGlx.cpp
@@ -0,0 +1,17 @@
+/*
+ * VisualFactoryGlx.cpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "ggl/window/glx/VisualFactoryGlx.hpp"
+using namespace Ggl;
+using namespace std;
+
+VisualFactoryGlx::VisualFactoryGlx() {
+    ;
+}
+
+VisualFactoryGlx::~VisualFactoryGlx() {
+    ;
+}
---
+++ src/ggl/window/glx/VisualFactoryGlx.hpp
@@ -0,0 +1,20 @@
+/*
+ * VisualFactoryGlx.hpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GGL_VISUALFACTORYGLX_HPP
+#define GGL_VISUALFACTORYGLX_HPP
+#include "ggl/common.h"
+namespace Ggl {
+
+
+class VisualFactoryGlx {
+public:
+    VisualFactoryGlx();
+    virtual ~VisualFactoryGlx();
+};
+
+} // namespace Ggl
+#endif // GGL_VISUALFACTORYGLX_HPP
