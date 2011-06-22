6c614f64ccc4ce9abf9248eab580e8237304b1e7
Andy Brown <adb1413@rit.edu>
2011-06-22 00:29:32 -0400

    Implement VisualFactoryGlx

--- src/ggl/window/glx/VisualFactoryGlx.hpp
+++ src/ggl/window/glx/VisualFactoryGlx.hpp
@@ -7,6 +7,8 @@
 #ifndef GGL_VISUALFACTORYGLX_HPP
 #define GGL_VISUALFACTORYGLX_HPP
 #include "ggl/common.h"
+#include "ggl/Exception.hpp"
+#include "ggl/window/WindowFormat.hpp"
 namespace Ggl {
 
 
@@ -14,6 +16,13 @@ class VisualFactoryGlx {
 public:
     VisualFactoryGlx();
     virtual ~VisualFactoryGlx();
+    XVisualInfo* createVisualInfo(const WindowFormat &wf);
+private:
+    Display *display;
+// Helpers
+    static Display* createDisplay() throw(std::exception);
+    static int* toArray(const WindowFormat &wf);
+    static std::list<int> toList(const WindowFormat &wf);
 };
 
 } // namespace Ggl
