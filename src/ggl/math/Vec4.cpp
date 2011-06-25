6f8ad5882dd0e694a84e9d3ef2a8cbcbc9660500
Andy Brown <adb1413@rit.edu>
2011-06-25 18:59:00 -0400

    Added more documentation to Vec4

--- src/ggl/math/Vec4.cpp
+++ src/ggl/math/Vec4.cpp
@@ -9,7 +9,7 @@ using namespace Ggl;
 using namespace std;
 
 /**
- * Creates an empty vector with a default homogeneous coordinate.
+ * Creates an empty vector with one for the homogeneous coordinate.
  */
 Vec4::Vec4() {
     x = 0.0;
@@ -19,11 +19,11 @@ Vec4::Vec4() {
 }
 
 /**
- * Creates a vector with explicit values and a default homogeneous coordinate.
+ * Creates a vector from values with one for the homogeneous coordinate.
  * 
- * @param x Value on X axis
- * @param y Value on Y axis
- * @param z Value on Z axis
+ * @param x Value for X component
+ * @param y Value for Y component
+ * @param z Value for Z component
  */
 Vec4::Vec4(float x, float y, float z) {
     this->x = x;
@@ -35,9 +35,9 @@ Vec4::Vec4(float x, float y, float z) {
 /**
  * Creates a vector with explicit values.
  * 
- * @param x Value on X axis
- * @param y Value on Y axis
- * @param z Value on Z axis
+ * @param x Value for X component
+ * @param y Value for Y component
+ * @param z Value for Z component
  * @param w Value for homogeneous coordinate
  */
 Vec4::Vec4(float x, float y, float z, float w) {
@@ -47,6 +47,12 @@ Vec4::Vec4(float x, float y, float z, float w) {
     this->w = w;
 }
 
+/**
+ * Copies the values of another vector to this vector.
+ * 
+ * @param v Vector to copy
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator=(const Vec4 &v) {
     
     // Check for self-assignment
@@ -62,6 +68,12 @@ Vec4& Vec4::operator=(const Vec4 &v) {
     return *this;
 }
 
+/**
+ * Adds another vector to this vector.
+ * 
+ * @param v Vector with values to add
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator+=(const Vec4 &v) {
     x += v.x;
     y += v.y;
@@ -70,6 +82,12 @@ Vec4& Vec4::operator+=(const Vec4 &v) {
     return *this;
 }
 
+/**
+ * Subtracts another vector from this vector.
+ * 
+ * @param v Vector with values to subtract
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator-=(const Vec4 &v) {
     x -= v.x;
     y -= v.y;
@@ -78,6 +96,12 @@ Vec4& Vec4::operator-=(const Vec4 &v) {
     return *this;
 }
 
+/**
+ * Multiplies this vector by another vector.
+ * 
+ * @param v Vector with values to multiply by
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator*=(const Vec4 &v) {
     x *= v.x;
     y *= v.y;
@@ -86,6 +110,12 @@ Vec4& Vec4::operator*=(const Vec4 &v) {
     return *this;
 }
 
+/**
+ * Divides this vector by another vector.
+ * 
+ * @param v Vector with values to divide by
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator/=(const Vec4 &v) {
     
     // Check for division by zero
@@ -100,6 +130,12 @@ Vec4& Vec4::operator/=(const Vec4 &v) {
     return *this;
 }
 
+/**
+ * Adds a value to this vector.
+ * 
+ * @param f Value to add
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator+=(float f) {
     x += f;
     y += f;
@@ -108,6 +144,12 @@ Vec4& Vec4::operator+=(float f) {
     return *this;
 }
 
+/**
+ * Subtracts a value from this vector.
+ * 
+ * @param f Value to subtract
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator-=(float f) {
     x -= f;
     y -= f;
@@ -116,6 +158,12 @@ Vec4& Vec4::operator-=(float f) {
     return *this;
 }
 
+/**
+ * Multiplies this vector by a value.
+ * 
+ * @param f Value to multiply by
+ * @return Reference to this vector
+ */
 Vec4& Vec4::operator*=(float f) {
     x *= f;
     y *= f;
@@ -124,6 +172,13 @@ Vec4& Vec4::operator*=(float f) {
     return *this;
 }
 
+/**
+ * Divides this vector by a value.
+ * 
+ * @param f Value to divide by
+ * @return Reference to this vector
+ * @throw std::exception if would divide by zero
+ */
 Vec4& Vec4::operator/=(float f) {
     
     float inv;
@@ -141,22 +196,58 @@ Vec4& Vec4::operator/=(float f) {
     return *this;
 }
 
+/**
+ * Checks if two vectors are equal.
+ * 
+ * @param u First vector to check
+ * @param v Second vector to check
+ * @return <tt>true</tt> if vectors are equals
+ */
 bool operator==(const Vec4 &u, const Vec4 &v) {
     return (u.x == v.x) && (u.y == v.y) && (u.z == v.z) && (u.w == v.w);
 }
 
+/**
+ * Adds a vector to another vector.
+ * 
+ * @param u Vector to add to
+ * @param v Vector to add
+ * @return Copy of resulting vector
+ */
 Vec4 operator+(const Vec4 &u, const Vec4 &v) {
     return Vec4((u.x + v.x), (u.y + v.y), (u.z + v.z), 1.0);
 }
 
+/**
+ * Subtracts a vector from another vector.
+ * 
+ * @param u Vector to subtract from
+ * @param v Vector to subtract
+ * @return Copy of resulting vector
+ */
 Vec4 operator-(const Vec4 &u, const Vec4 &v) {
     return Vec4((u.x - v.x), (u.y - v.y), (u.z - v.z), 1.0);
 }
 
+/**
+ * Multiplies a vector by another vector.
+ * 
+ * @param u Vector to multiply
+ * @param v Vector to multiply by
+ * @return Copy of resulting vector
+ */
 Vec4 operator*(const Vec4 &u, const Vec4 &v) {
     return Vec4((u.x * v.x), (u.y * v.y), (u.z * v.z), 1.0);
 }
 
+/**
+ * Divides a vector by another vector.
+ * 
+ * @param u Vector to divide
+ * @param v Vector to divide by
+ * @return Copy of resulting vector
+ * @throw std::exception if would divide by zero
+ */
 Vec4 operator/(const Vec4 &u, const Vec4 &v) {
     if ((v.x == 0) || (v.y == 0) || (v.z == 0)) {
         throw Exception("[Vec4] Cannot divide by zero!");
@@ -165,18 +256,47 @@ Vec4 operator/(const Vec4 &u, const Vec4 &v) {
     }
 }
 
+/**
+ * Adds a value to a vector.
+ * 
+ * @param u Vector to add to
+ * @param f Value to add
+ * @return Copy of resulting vector
+ */
 Vec4 operator+(const Vec4 &u, float f) {
     return Vec4((u.x + f), (u.y + f), (u.z + f), 1.0);
 }
 
+/**
+ * Subtracts a value from a vector.
+ * 
+ * @param u Vector to subtract from
+ * @param f Value to subtract
+ * @return Copy of resulting vector
+ */
 Vec4 operator-(const Vec4 &u, float f) {
     return Vec4((u.x - f), (u.y - f), (u.z - f), 1.0);
 }
 
+/**
+ * Multiplies a vector by a value.
+ * 
+ * @param u Vector to multiply
+ * @param f Value to multiply by
+ * @return Copy of resulting vector
+ */
 Vec4 operator*(const Vec4 &u, float f) {
     return Vec4((u.x * f), (u.y * f), (u.z * f), 1.0);
 }
 
+/**
+ * Divides a vector by a value.
+ * 
+ * @param u Vector to divide
+ * @param f Value to divide by
+ * @return Copy of resulting vector
+ * @throw std::exception if would divide by zero
+ */
 Vec4 operator/(const Vec4 &u, float f) {
     
     float inv;
@@ -189,6 +309,13 @@ Vec4 operator/(const Vec4 &u, float f) {
     }
 }
 
+/**
+ * Appends a vector to a stream.
+ * 
+ * @param out Stream to append to
+ * @param u Vector to append
+ * @return Reference to the stream
+ */
 ostream& operator<<(ostream &out, const Vec4 &u) {
     out << fixed << setprecision(2);
     out << "[";
@@ -200,7 +327,11 @@ ostream& operator<<(ostream &out, const Vec4 &u) {
 }
 
 /**
- * @throw Exception if the index is out of bounds.
+ * Accesses a component in the vector by index.
+ * 
+ * @param i Index of component
+ * @return Reference to the component
+ * @throw std::exception if the index is out of bounds.
  */
 float& Vec4::operator[](int i) {
     switch (i) {
@@ -214,7 +345,11 @@ float& Vec4::operator[](int i) {
 }
 
 /**
- * @throw BasicException if the index is out of bounds.
+ * Accesses a component in the vector by index.
+ * 
+ * @param i Index of component
+ * @return Copy of the component
+ * @throw std::exception if the index is out of bounds.
  */
 float Vec4::operator[](int i) const {
     switch (i) {
@@ -228,7 +363,11 @@ float Vec4::operator[](int i) const {
 }
 
 /**
- * @return vector perpendicular to the plane formed by two vectors.
+ * Computes the cross product of two vectors.
+ * 
+ * @param u First vector
+ * @param v Second vector
+ * @return vector perpendicular to the plane formed by two vectors
  */
 Vec4 cross(const Vec4 &u, const Vec4 &v) {
     
@@ -240,14 +379,22 @@ Vec4 cross(const Vec4 &u, const Vec4 &v) {
 }
 
 /**
- * @return projection of one vector onto another.
+ * Computes the dot product of two vectors.
+ * 
+ * @param u First vector
+ * @param v Second vector
+ * @return projection of one vector onto another
  */
 float dot(const Vec4 &u, const Vec4 &v) {
     return (u.x * v.x) + (u.y * v.y) + (u.z * v.z);
 }
 
 /**
- * @throw BasicException if index out of bounds.
+ * Accesses a component of the vector.
+ * 
+ * @param i Index of component
+ * @return Copy of the component's value
+ * @throw std::exception if index out of bounds.
  */
 float Vec4::get(int i) const {
     switch (i) {
@@ -261,28 +408,41 @@ float Vec4::get(int i) const {
 }
 
 /**
- * @return length of the vector.
+ * Calculates the length of the vector.
+ * 
+ * @return Length of the vector
  */
 float Vec4::length() const {
     return sqrt((x * x) + (y * y) + (z * z));
 }
 
 /**
- * @return component-wise maximum of the two vectors
+ * Computes a component-wise maximum of two vectors.
+ * 
+ * @param u First vector
+ * @param v Second vector
+ * @return Copy of resulting vector
  */
 Vec4 max(const Vec4 &u, const Vec4 &v) {
     return Vec4(max(u.x, v.x), max(u.y, v.y), max(u.z, v.z));
 }
 
 /**
- * @return component-wise minimum of the two vectors
+ * Computes a component-wise minimum of two vectors.
+ * 
+ * @param u First vector
+ * @param v Second vector
+ * @return Copy of resulting vector
  */
 Vec4 min(const Vec4 &u, const Vec4 &v) {
     return Vec4(min(u.x, v.x), min(u.y, v.y), min(u.z, v.z));
 }
 
 /**
- * @return Unit length version of the vector.
+ * Converts a vector to a unit-length vector.
+ * 
+ * @param u Vector to convert
+ * @return Copy of resulting vector
  */
 Vec4 normalize(const Vec4 &u) {
     
@@ -297,12 +457,27 @@ Vec4 normalize(const Vec4 &u) {
     }
 }
 
+/**
+ * Changes the values of a vector.
+ * 
+ * @param x Value for X component
+ * @param y Value for Y component
+ * @param z Value for Z component
+ */
 void Vec4::set(float x, float y, float z) {
     this->x = x;
     this->y = y;
     this->z = z;
 }
 
+/**
+ * Changes the values of a vector.
+ * 
+ * @param x Value for X component
+ * @param y Value for Y component
+ * @param z Value for Z component
+ * @param w Value for homogeneous coordinate
+ */
 void Vec4::set(float x, float y, float z, float w) {
     this->x = x;
     this->y = y;
@@ -310,6 +485,11 @@ void Vec4::set(float x, float y, float z, float w) {
     this->w = w;
 }
 
+/**
+ * Converts a vector to an array.
+ * 
+ * @param array Existing float array
+ */
 void Vec4::toArray(float array[4]) {
     array[0] = x;
     array[1] = y;
