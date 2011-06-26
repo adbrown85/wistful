d71955e9418eb3c0dc34deb26cf754ad5f4288d0
Andy Brown <adb1413@rit.edu>
2011-06-26 00:30:48 -0400

    Added TestCase

---
+++ src/ggl/testing/TestCase.cpp
@@ -0,0 +1,7 @@
+/*
+ * TestCase.cpp
+ *
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "ggl/testing/TestCase.hpp"
---
+++ src/ggl/testing/TestCase.hpp
@@ -0,0 +1,60 @@
+/*
+ * TestCase.hpp
+ *
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GGL_TESTCASE_HPP
+#define GGL_TESTCASE_HPP
+#include "ggl/common.h"
+#include "ggl/testing/TestFixture.hpp"
+namespace Ggl {
+
+
+/**
+ * @brief Part of a test that can be called.
+ */
+template <class T>
+class TestCase {
+public:
+    TestCase(const std::string &name, void (T::*method)());
+    void run(T &fixture);
+private:
+    std::string name;
+    void (T::*method)();
+};
+
+/**
+ * Creates a test case.
+ * 
+ * @param name Descriptive name
+ * @param method Method to call on fixture
+ */
+template<class T>
+inline
+TestCase<T>::TestCase(const std::string &name,
+                      void (T::*method)()) {
+    this->name = name;
+    this->method = method;
+}
+
+/**
+ * Runs the test case on a fixture.
+ * 
+ * @param fixture Test fixture to run test case on
+ */
+template<class T>
+inline
+void TestCase<T>::run(T &fixture) {
+    std::cout << "RUNNING " << name << std::endl;
+    try {
+        (fixture.*(method))();
+    } catch (std::exception &e) {
+        std::cout << "FAILED!" << std::endl;
+        exit(1);
+    }
+    std::cout << "PASSED" << std::endl;
+}
+
+}
+#endif
