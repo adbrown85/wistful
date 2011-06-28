9e05c9b326af5329ca2b8d96cedafda8c92263ff
Andy Brown <adb1413@rit.edu>
2011-06-28 00:02:39 -0400

    Replace tabs with spaces in Mat4 and Vec4 tests

--- test/ggl/math/Mat4Test.cxx
+++ test/ggl/math/Mat4Test.cxx
@@ -18,12 +18,12 @@ using namespace Ggl;
  */
 class Mat4Test : public TestFixture {
 public:
-	void setUp();
-	void testInverse();
-	void testMultiplyTime();
-	void testSetArray();
-	void testTranspose();
-	void testMultiplyVector();
+    void setUp();
+    void testInverse();
+    void testMultiplyTime();
+    void testSetArray();
+    void testTranspose();
+    void testMultiplyVector();
 private:
 	Mat4 m1, m2, m3;
 };
@@ -35,47 +35,47 @@ void Mat4Test::setUp() {
                         {1.0, 6.0, 4.0, 5.0},
                         {1.0, 2.0, 3.0, 4.0} };
     
-	m1 = Mat4(arr);
-	m2 = Mat4(1.0);
-	m3 = Mat4(1.0);
+    m1 = Mat4(arr);
+    m2 = Mat4(1.0);
+    m3 = Mat4(1.0);
 }
 
 void Mat4Test::testInverse() {
-	
-	m2 = inverse(m1);
-	m3 = m1 * m2;
-	
-	for (int i=0; i<4; ++i) {
-		assertEquals(m3(i,i), 1.0);
-	}
-	
-	for (int i=0; i<3; ++i) {
-		for (int j=i+1; j<4; ++j) {
-			assertEquals(m3(i,j), 0.0);
-		}
-	}
-	
-	for (int i=1; i<4; ++i) {
-		for (int j=0; j<i; ++j) {
-			assertEquals(m3(i,j), 0.0);
-		}
-	}
+    
+    m2 = inverse(m1);
+    m3 = m1 * m2;
+    
+    for (int i=0; i<4; ++i) {
+        assertEquals(m3(i,i), 1.0);
+    }
+    
+    for (int i=0; i<3; ++i) {
+        for (int j=i+1; j<4; ++j) {
+            assertEquals(m3(i,j), 0.0);
+        }
+    }
+    
+    for (int i=1; i<4; ++i) {
+        for (int j=0; j<i; ++j) {
+            assertEquals(m3(i,j), 0.0);
+        }
+    }
 }
 
 void Mat4Test::testSetArray() {
-	
-	float arr[16] = { 1.0,  2.0,  3.0,  4.0,
-	                  5.0,  6.0,  7.0,  8.0,
-	                  9.0, 10.0, 11.0, 12.0,
-	                 13.0, 14.0, 15.0, 16.0};
-	
-	m2 = Mat4(arr);
-	
-	for (int i=0; i<4; ++i) {
-		for (int j=0; j<4; ++j) {
-			assertEquals(arr[i*4+j], m2(j,i));
-		}
-	}
+    
+    float arr[16] = { 1.0,  2.0,  3.0,  4.0,
+                      5.0,  6.0,  7.0,  8.0,
+                      9.0, 10.0, 11.0, 12.0,
+                     13.0, 14.0, 15.0, 16.0};
+    
+    m2 = Mat4(arr);
+    
+    for (int i=0; i<4; ++i) {
+        for (int j=0; j<4; ++j) {
+            assertEquals(arr[i*4+j], m2(j,i));
+        }
+    }
 }
 
 void Mat4Test::testTranspose() {
@@ -86,24 +86,24 @@ void Mat4Test::testTranspose() {
                         {7.0, 6.0, 5.0, 4.0}};
     
     m2 = Mat4(arr);
-	m3 = transpose(m1);
-	
-	for (int i=0; i<4; ++i) {
-	    for (int j=0; j<4; ++j) {
-	        assertEquals(m2(i,j), m3(i,j));
-	    }
-	}
+    m3 = transpose(m1);
+    
+    for (int i=0; i<4; ++i) {
+        for (int j=0; j<4; ++j) {
+            assertEquals(m2(i,j), m3(i,j));
+        }
+    }
 }
 
 void Mat4Test::testMultiplyVector() {
-	
-	Vec4 u(8.0, 3.0, 1.0, 7.0);
-	Vec4 v = m1 * u;
-	
-	assertEquals(101, v.x);
-	assertEquals( 76, v.y);
-	assertEquals( 65, v.z);
-	assertEquals( 45, v.w);
+    
+    Vec4 u(8.0, 3.0, 1.0, 7.0);
+    Vec4 v = m1 * u;
+    
+    assertEquals(101, v.x);
+    assertEquals( 76, v.y);
+    assertEquals( 65, v.z);
+    assertEquals( 45, v.w);
 }
 
 #define GGL_TEST_FIXTURE Mat4Test
--- test/ggl/math/Vec4Test.cxx
+++ test/ggl/math/Vec4Test.cxx
@@ -18,49 +18,49 @@ using namespace Ggl;
 class Vec4Test : public TestFixture {
 public:
     void setUp();
-	void testAdd();
-	void testSubtract();
-	void testMultiply();
-	void testDivide();
+    void testAdd();
+    void testSubtract();
+    void testMultiply();
+    void testDivide();
 private:
-	Vec4 A, B, C;
+    Vec4 A, B, C;
 };
 
 void Vec4Test::setUp() {
-	A.set(2, 2, 2);
-	B.set(2, 4, 5);
+    A.set(2, 2, 2);
+    B.set(2, 4, 5);
 }
 
 void Vec4Test::testAdd() {
-	
-	C = A + B;
-	assertEquals(4, C.x);
-	assertEquals(6, C.y);
-	assertEquals(7, C.z);
+    
+    C = A + B;
+    assertEquals(4, C.x);
+    assertEquals(6, C.y);
+    assertEquals(7, C.z);
 }
 
 void Vec4Test::testSubtract() {
-	
-	C = A - B;
-	assertEquals( 0, C.x);
-	assertEquals(-2, C.y);
-	assertEquals(-3, C.z);
+    
+    C = A - B;
+    assertEquals( 0, C.x);
+    assertEquals(-2, C.y);
+    assertEquals(-3, C.z);
 }
 
 void Vec4Test::testMultiply() {
-	
-	C = A * B;
-	assertEquals( 4, C.x);
-	assertEquals( 8, C.y);
-	assertEquals(10, C.z);
+    
+    C = A * B;
+    assertEquals( 4, C.x);
+    assertEquals( 8, C.y);
+    assertEquals(10, C.z);
 }
 
 void Vec4Test::testDivide() {
-	
-	C = A / B;
-	assertEquals(1.0, C.x);
-	assertEquals(0.5, C.y);
-	assertEquals(0.4, C.z);
+    
+    C = A / B;
+    assertEquals(1.0, C.x);
+    assertEquals(0.5, C.y);
+    assertEquals(0.4, C.z);
 }
 
 #define GGL_TEST_FIXTURE Vec4Test
