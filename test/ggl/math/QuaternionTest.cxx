a3b62fee0d11436458de982d659eab370d491760
Andy Brown <adb1413@rit.edu>
2011-06-27 23:25:11 -0400

    Use assertions in QuaternionTest

--- test/ggl/math/QuaternionTest.cxx
+++ test/ggl/math/QuaternionTest.cxx
@@ -15,25 +15,58 @@ using namespace Ggl;
  */
 class QuaternionTest : public TestFixture {
 public:
-	void testToMat4();
-	void testRotate();
+    void testToMat4();
+    void testRotate();
 private:
-	Mat4 mat;
-	Quaternion q;
+    Mat4 mat;
+    Quaternion q;
 };
 
 void QuaternionTest::testToMat4() {
-	q = Quaternion(45, Vec4(0,1,0));
-	print(q);
-	mat = q.toMat4();
-	print(mat);
+    
+    q = Quaternion(45, Vec4(0,1,0));
+    mat = q.toMat4();
+    
+    assertEquals( 0.707, mat(0,0));
+    assertEquals( 0.000, mat(0,1));
+    assertEquals( 0.707, mat(0,2));
+    assertEquals( 0.000, mat(0,3));
+    assertEquals( 0.000, mat(1,0));
+    assertEquals( 1.000, mat(1,1));
+    assertEquals( 0.000, mat(1,2));
+    assertEquals( 0.000, mat(1,3));
+    assertEquals(-0.707, mat(2,0));
+    assertEquals( 0.000, mat(2,1));
+    assertEquals( 0.707, mat(2,2));
+    assertEquals( 0.000, mat(2,3));
+    assertEquals( 0.000, mat(3,0));
+    assertEquals( 0.000, mat(3,1));
+    assertEquals( 0.000, mat(3,2));
+    assertEquals( 1.000, mat(3,3));
 }
 
 void QuaternionTest::testRotate() {
-	q = Quaternion(30, Vec4(0,1,0));
-	q.rotate(45, Vec4(0,1,0));
-	mat = q.toMat4();
-	print(mat);
+    
+    q = Quaternion(30, Vec4(0,1,0));
+    q.rotate(45, Vec4(0,1,0));
+    mat = q.toMat4();
+    
+    assertEquals( 0.259, mat(0,0));
+    assertEquals( 0.000, mat(0,1));
+    assertEquals( 0.966, mat(0,2));
+    assertEquals( 0.000, mat(0,3));
+    assertEquals( 0.000, mat(1,0));
+    assertEquals( 1.000, mat(1,1));
+    assertEquals( 0.000, mat(1,2));
+    assertEquals( 0.000, mat(1,3));
+    assertEquals(-0.966, mat(2,0));
+    assertEquals( 0.000, mat(2,1));
+    assertEquals( 0.259, mat(2,2));
+    assertEquals( 0.000, mat(2,3));
+    assertEquals( 0.000, mat(3,0));
+    assertEquals( 0.000, mat(3,1));
+    assertEquals( 0.000, mat(3,2));
+    assertEquals( 1.000, mat(3,3));
 }
 
 /*
