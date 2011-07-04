69facc9eb3047668d70a4e0be1390f958ed4710b
Andy Brown <adb1413@rit.edu>
2011-07-04 18:55:33 -0400

    BufferObject

---
+++ src/ggl/geometry/BufferObject.cpp
@@ -0,0 +1,85 @@
+/*
+ * BufferObject.cpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "ggl/geometry/BufferObject.hpp"
+using namespace std;
+using namespace Ggl;
+
+/**
+ * Creates a new buffer object.
+ */
+BufferObject::BufferObject(GLenum type) {
+	this->type = type;
+	this->handle = createHandle();
+}
+
+/**
+ * Destroys the buffer object.
+ */
+BufferObject::~BufferObject() {
+	glDeleteBuffers(1, &handle);
+}
+
+/**
+ * Activates the buffer object.
+ */
+void BufferObject::bind() {
+	glBindBuffer(type, handle);
+}
+
+/**
+ * Stops using the buffer object.
+ */
+void BufferObject::unbind() {
+	glBindBuffer(type, 0);
+}
+
+/**
+ * Allocates enough room for the VBO.
+ */
+void BufferObject::allocate(GLenum usage, GLsizei size) {
+	glBufferData(type,                // target
+				 size,                // size
+				 NULL,                // data
+				 usage);              // usage
+}
+
+/**
+ * Fills the buffer with data.
+ */
+void BufferObject::update(GLsizei size, GLvoid *data, int offset) {
+	glBufferSubData(type,             // target
+	                offset,           // offset
+	                size,             // size
+	                data);            // data
+}
+
+/**
+ * Returns a handle to a new buffer.
+ */
+GLuint BufferObject::createHandle() {
+    
+    GLuint handle;
+    
+    glGenBuffers(1, &handle);
+    return handle;
+}
+
+// GETTERS AND SETTERS
+
+/**
+ * Returns OpenGL's underlying identifier for the buffer.
+ */
+GLuint BufferObject::getHandle() const {
+    return handle;
+}
+
+/**
+ * Returns type of the buffer.
+ */
+GLenum BufferObject::getType() const {
+    return type;
+}
---
+++ src/ggl/geometry/BufferObject.hpp
@@ -0,0 +1,37 @@
+/*
+ * BufferObject.hpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GGL_BUFFEROBJECT_HPP
+#define GGL_BUFFEROBJECT_HPP
+#include "ggl/common.h"
+#include "ggl/Exception.hpp"
+namespace Ggl {
+
+
+/**
+ * @brief OpenGL buffer object.
+ */
+class BufferObject {
+public:
+	BufferObject(GLenum type);
+	virtual ~BufferObject();
+	virtual void bind();
+	virtual void unbind();
+// Getters and setters
+	virtual GLuint getHandle() const;
+	virtual GLenum getType() const;
+protected:
+	virtual void allocate(GLenum usage, GLsizei size);
+	virtual void update(GLsizei size, GLvoid *data, int offset=0);
+private:
+	GLuint handle;
+	GLenum type;
+// Helpers
+	static GLuint createHandle();
+};
+
+} /* namespace Ggl */
+#endif
