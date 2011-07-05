86c45f7d621d538388542b43823e821a3708d404
Andy Brown <adb1413@rit.edu>
2011-07-05 02:02:29 -0400

    ErrorChecker

---
+++ src/ggl/system/ErrorChecker.hpp
@@ -0,0 +1,27 @@
+/*
+ * ErrorChecker.hpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GGL_ERRORCHECKER_HPP
+#define GGL_ERRORCHECKER_HPP
+#include "ggl/common.h"
+#include "ggl/Exception.hpp"
+namespace Ggl {
+
+
+/**
+ * Utility for checking for OpenGL errors.
+ */
+class ErrorChecker {
+public:
+	static void checkErrors(const std::string &location);
+	static void clearErrors();
+private:
+	static void report(GLenum error, const std::string &location);
+	static std::string toString(GLenum error);
+};
+
+} /* namespace Ggl */
+#endif
