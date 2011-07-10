e7270b28f7ded45f4c6a4e0abc1c905c0e44a095
Andy Brown <adb1413@rit.edu>
2011-07-09 23:33:53 -0400

    Added WindowAdapter

---
+++ src/ggl/window/WindowAdapter.cpp
@@ -0,0 +1,39 @@
+/*
+ * WindowAdapter.cpp
+ *
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#include "ggl/window/WindowAdapter.hpp"
+using namespace std;
+using namespace Ggl;
+
+/**
+ * Constructs a window adapter.
+ */
+WindowAdapter::WindowAdapter() {
+    ;
+}
+
+/**
+ * Destroys the window adapter.
+ */
+WindowAdapter::~WindowAdapter() {
+    ;
+}
+
+void WindowAdapter::onDestroy(Window *window) {
+    ;
+}
+
+void WindowAdapter::onDisplay(Window *window) {
+    ;
+}
+
+void WindowAdapter::onInit(Window *window) {
+    ;
+}
+
+void WindowAdapter::onKey(Window *window, const WindowEvent &we) {
+    ;
+}
